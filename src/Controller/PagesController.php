97c86dcf39b1d19e6c716124272bded223732ab5
Guillaume GOMEZ <guillaumegomez@MacBook-Air-de-Guillaume.local>
2020-07-28 10:57:49 +0200

    reinit

---
+++ src/Controller/PagesController.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class PagesController extends AbstractController {
+
+    /**
+     * @Route ("/", name = "Accueil")
+     */
+    public function Accueil () {
+
+        return $this->render("Pages/page-accueil.html.twig");
+    }
+
+    /**
+     * @Route ("/peintures", name = "Peintures")
+     */
+    public function Peintures () {
+
+        return $this->render("Pages/interieur-exterieur.html.twig");
+    }
+
+    /**
+     * @Route ("/toiture", name = "Toiture")
+     */
+    public function Toiture () {
+
+        return $this->render('Pages/demoussage-toiture.html.twig');
+    }
+
+    /**
+     * @Route ("/travaux", name = "Travaux")
+     */
+    public function Travaux () {
+
+        return $this->render('Pages/travaux.html.twig');
+    }
+
+    /**
+     * @Route ("/realisation", name = "Réalisations")
+     */
+    public function Réalisation () {
+
+        return $this->render('Pages/mes-realisations.html.twig');
+    }
+
+    /**
+     * @Route ("/contact", name = "Contact")
+     */
+    public function Contact () {
+
+        return $this->render('Pages/contact.html.twig');
+    }
+
+    /**
+     * @Route ("/espace_utilisateur", name = "Connexion_Inscription")
+     */
+    public function espace_utilisateur () {
+
+        return $this->render('Pages/connexion-inscription.html.twig');
+    }
+
+
+}
