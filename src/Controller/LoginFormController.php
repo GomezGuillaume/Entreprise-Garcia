2408696ca9abecd54068cd1c57bde0eceff5dd07
GomezGuillaume <guillaume.gomez@lapiscine.pro>
2020-08-27 17:14:05 +0200

    modify content and form login

--- src/Controller/LoginFormController.php
+++ src/Controller/LoginFormController.php
@@ -14,9 +14,11 @@ class LoginFormController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+         if ($this->getUser())
+         {
+             return $this->redirectToRoute('realisations');
+         }
+
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
